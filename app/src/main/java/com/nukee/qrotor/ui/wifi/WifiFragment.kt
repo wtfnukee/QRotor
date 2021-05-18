3af25b7b7d7699e098ce3ee9bcb9b119213ad075
wtfnukee <wtfnukee@gmail.com>
2021-05-18 18:31:55 +0300

    Add Listener

--- app/src/main/java/com/nukee/qrotor/ui/wifi/WifiFragment.kt
+++ app/src/main/java/com/nukee/qrotor/ui/wifi/WifiFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.nukee.qrotor.R
 import com.nukee.qrotor.databinding.FragmentWifiBinding
 import com.nukee.qrotor.getQrCodeBitmap
 import kotlinx.android.synthetic.main.fragment_wifi.*
@@ -37,11 +39,9 @@ class WifiFragment : Fragment() {
         wifiViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
-        
-        generateBtn.setOnClickListener(){
-            fun onClick(){
-                qrCodeImage.setImageBitmap(getQrCodeBitmap("f"))
-            }
+
+        view?.findViewById<Button>(R.id.generateBtn)?.setOnClickListener{
+            qrCodeImage.setImageBitmap(getQrCodeBitmap("f"))
         }
 
         return root
@@ -51,5 +51,6 @@ class WifiFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
 
 }
